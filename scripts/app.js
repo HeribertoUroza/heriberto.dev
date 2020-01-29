2560646487788e8e4b7d225d5ac4bbdbce6eceb3
Heriberto Uroza <heribertouroza@pursuit.org>
2020-01-28 22:17:08 -0500

    smooth scrolling working, need to add if condition because other anchor tags are not working

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
 
     $("a").on("click", function(event) {
         console.log(this.hash)
+        event.preventDefault();
         
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
     })
 })
