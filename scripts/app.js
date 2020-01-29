bd65514bb231a08aacb4b902ec5d36ce43022e6b
Heriberto Uroza <heribertouroza@pursuit.org>
2020-01-29 18:52:31 -0500

    observer working, is able to confirm if container is visable.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,11 @@
 // VARIABLES
-const tieColor = document.getElementById('tieColor')
+const tieColor = document.getElementById('tieColor');
+const titles = document.querySelectorAll('#page');
+const arrColors = [ "url('https://images.unsplash.com/photo-1500349812227-3264f5f54181?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')", 
+                    "url('https://images.unsplash.com/photo-1571028634586-ae87c1a42432?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')", 
+                    "url('https://images.unsplash.com/photo-1580298323453-15d7e7323c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')", 
+                    "url('https://images.unsplash.com/photo-1580236244970-2adb498f47da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')" 
+                ];
 
 // SMOOTH SCROLLING
 $(document).ready(function(){
@@ -47,3 +53,26 @@ $(document).ready(function(){
         
     });
 });
+
+// TIE COLOR CHANGE
+const options = {
+    threshold: 0.7,
+};
+
+const pageCheck = entries => {
+    entries.forEach( entry => {
+        console.log('page', entry.isIntersecting)
+    
+        if(entry.isIntersecting){
+            tieColor.style.backgroundImage = "url('https://images.unsplash.com/photo-1500349812227-3264f5f54181?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')"
+        }
+    })
+};
+
+let observer = new IntersectionObserver(pageCheck, options);
+
+titles.forEach( title => {
+    observer.observe(title);
+})
+
+console.log('titles',titles)
